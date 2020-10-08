bb6829281b9effd5b70f9ac9a14ce9f4811da6dd
Lucina <10523871+Lucina@users.noreply.github.com>
2020-10-08 00:35:00 -0700

    Update build scripts

--- templates/src/fpt/publish_osx-x64_mini.sh
+++ templates/src/fpt/publish_osx-x64_mini.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dotnet publish "$DIR" -f net5.0 -c Release -r osx-x64 -o "$DIR/publish" -v m /p:IlcDisableReflection=true /p:IlcInvariantGlobalization=true /p:RootAllApplicationAssemblies=false /p:IlcGenerateCompleteTypeMetadata=false /p:IlcGenerateStackTraceData=false /p:IlcOptimizationPreference=Speed
+dotnet publish "$DIR" -f net5.0 -c Release -r osx-x64 -o "$DIR/publish" -v m /p:IlcDisableReflection=true /p:IlcInvariantGlobalization=true /p:RootAllApplicationAssemblies=false /p:IlcGenerateCompleteTypeMetadata=false /p:IlcGenerateStackTraceData=false /p:IlcOptimizationPreference=Speed /p:DebugType=None /p:DebugSymbols=false
 strip $DIR/publish/${DIR##*/}
